7ff37ee42300e21f7014fd1ddaa5ef41ba5aabdf
zjsywcc <zjsywcc@163.com>
2016-07-15 22:38:20 +0800

    second commit

--- src/main/java/com/emusicstore/controller/HomeController.java
+++ src/main/java/com/emusicstore/controller/HomeController.java
@@ -4,8 +4,10 @@ import com.emusicstore.dao.ProductDao;
 import com.emusicstore.model.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.io.IOException;
 import java.util.List;
 
 /**
@@ -25,6 +27,14 @@ public class HomeController {
         List<Product> products = productDao.getProductList();
         model.addAttribute("products", products);
         return "productList";
+    }
+
+    @RequestMapping("/productList/viewProduct/{productId}")
+    public String viewProduct(@PathVariable String productId, Model model) throws IOException{
+
+        Product product = productDao.getProductById(productId);
+        model.addAttribute("product",product);
+        return "viewProduct";
     }
 
 }
--- src/main/java/com/emusicstore/model/Product.java
+++ src/main/java/com/emusicstore/model/Product.java
@@ -5,6 +5,7 @@ package com.emusicstore.model;
  */
 public class Product {
 
+    private String productId;
     private String productName;
     private String productCategory;
     private String productDescription;
@@ -13,6 +14,15 @@ public class Product {
     private String productStatus;
     private int unitInStock;
     private String productManufacturer;
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
 
     public String getProductName() {
         return productName;
